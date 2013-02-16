67273c62db24155916876032c5c00c31310865bf
SammysHP <sven@sammyshp.de>
2013-02-16 13:17:28 +0100

    Improve texttools
    - use jQuery
    - write some comments
    - add some more buttons
    - cleanup code

--- js/texttools.js
+++ js/texttools.js
@@ -1,4 +1,11 @@
-// from http://stackoverflow.com/questions/3964710/replacing-selected-text-in-the-textarea
+/**
+ * Get the selection range of an element.
+ *
+ * Requires a non-jQuery object, so use $(el)[0] in conjunction with jQuery.
+ *
+ * @source http://stackoverflow.com/questions/3964710/replacing-selected-text-in-the-textarea
+ * @return {start, end} with start <= end
+ */
 function getInputSelection(el) {
     if (el == null) {
         return {
@@ -6,47 +13,57 @@ function getInputSelection(el) {
             end: 0
         };
     }
-    var start = 0, end = 0, normalizedValue, range,
-        textInputRange, len, endRange;
-    if (typeof el.selectionStart == "number" && typeof el.selectionEnd == "number") {
+
+    var start = 0;
+    var end = 0;
+
+    if (typeof el.selectionStart == 'number' && typeof el.selectionEnd == 'number') {
         start = el.selectionStart;
         end = el.selectionEnd;
     } else {
-        range = document.selection.createRange();
+        var range = document.selection.createRange();
         if (range && range.parentElement() == el) {
-            len = el.value.length;
-            normalizedValue = el.value.replace(/\r\n/g, "\n");
-            textInputRange = el.createTextRange();
+            var len = el.value.length;
+            var normalizedValue = el.value.replace(/\r\n/g, "\n");
+            var textInputRange = el.createTextRange();
             textInputRange.moveToBookmark(range.getBookmark());
-            endRange = el.createTextRange();
+            var endRange = el.createTextRange();
             endRange.collapse(false);
 
-            if (textInputRange.compareEndPoints("StartToEnd", endRange) > -1) {
+            if (textInputRange.compareEndPoints('StartToEnd', endRange) > -1) {
                 start = end = len;
             } else {
-                start = -textInputRange.moveStart("character", -len);
+                start = -textInputRange.moveStart('character', -len);
                 start += normalizedValue.slice(0, start).split("\n").length - 1;
 
-                if (textInputRange.compareEndPoints("EndToEnd", endRange) > -1) {
+                if (textInputRange.compareEndPoints('EndToEnd', endRange) > -1) {
                     end = len;
                 } else {
-                    end = -textInputRange.moveEnd("character", -len);
+                    end = -textInputRange.moveEnd('character', -len);
                     end += normalizedValue.slice(0, end).split("\n").length - 1;
                 }
             }
         }
     }
+
     return {
         start: start,
         end: end
     };
 }
 
-// from http://stackoverflow.com/questions/512528/set-cursor-position-in-html-textbox
+/**
+ * Set the caret position in input or textarea.
+ *
+ * Requires a non-jQuery object, so use $(el)[0] in conjunction with jQuery.
+ *
+ * @source http://stackoverflow.com/questions/512528/set-cursor-position-in-html-textbox
+ */
 function setCaretPosition(el, pos) {
     if (el == null) {
         return false;
     }
+
     if (el.createTextRange) {
         var range = el.createTextRange();
         range.move('character', pos);
@@ -61,11 +78,11 @@ function setCaretPosition(el, pos) {
     }
 }
 
-function simpleEscape(text) {
+function simpleHtmlEscape(text) {
     return String(text).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
 }
 
-function simpleUnescape(text) {
+function simpleHtmlUnescape(text) {
     return String(text).replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&amp;/g, '&');
 }
 
@@ -73,85 +90,95 @@ function surroundSelectedText(el, before, after) {
     var sel = getInputSelection(el)
     var val = el.value;
     var text = val.slice(sel.start, sel.end);
+    var scrollposition = el.scrollTop;
     el.value = val.slice(0, sel.start) + before + text + after + val.slice(sel.end);
+    el.scrollTop = scrollposition;
     setCaretPosition(el, sel.start + before.length + text.length);
-    return false;
 }
 
 function escapeSelectedText(el) {
     var sel = getInputSelection(el)
     var val = el.value;
-    var text = simpleEscape(val.slice(sel.start, sel.end));
+    var text = simpleHtmlEscape(val.slice(sel.start, sel.end));
+    var scrollposition = el.scrollTop;
     el.value = val.slice(0, sel.start) + text + val.slice(sel.end);
+    el.scrollTop = scrollposition;
     setCaretPosition(el, sel.start + text.length);
-    return false;
 }
 
 function unescapeSelectedText(el) {
     var sel = getInputSelection(el)
     var val = el.value;
-    var text = simpleUnescape(val.slice(sel.start, sel.end));
+    var text = simpleHtmlUnescape(val.slice(sel.start, sel.end));
+    var scrollposition = el.scrollTop;
     el.value = val.slice(0, sel.start) + text + val.slice(sel.end);
+    el.scrollTop = scrollposition;
     setCaretPosition(el, sel.start + text.length);
-    return false;
 }
 
 function replaceSelectedText(el, text) {
     var sel = getInputSelection(el);
     var val = el.value;
+    var scrollposition = el.scrollTop;
     el.value = val.slice(0, sel.start) + text + val.slice(sel.end);
+    el.scrollTop = scrollposition;
     setCaretPosition(el, sel.start + text.length);
-    return false;
 }
 
 function makeBold(el) {
     surroundSelectedText(el, '<b>', '</b>');
-    return false;
 }
 
 function makeItalic(el) {
     surroundSelectedText(el, '<i>', '</i>');
-    return false;
 }
 
 function makeUnderline(el) {
     surroundSelectedText(el, '<u>', '</u>');
-    return false;
 }
 
-function insertBreak(el) {
+function makeBreak(el) {
     surroundSelectedText(el, '<br />\n', '');
-    return false;
 }
 
-function insertParagraph(el) {
+function makeParagraph(el) {
     surroundSelectedText(el, '<p>\n', '\n</p>');
-    return false;
 }
 
-function makeAnchor(el) {
+function makeLink(el) {
     surroundSelectedText(el, '<a href="">', '</a>');
-    return false;
 }
 
 function makeImage(el) {
     surroundSelectedText(el, '<img src="', '" />');
-    return false;
 }
 
 function makeHeading(el) {
     surroundSelectedText(el, '<h3>', '</h3>');
-    return false;
+}
+
+function makeUnorderedList(el) {
+    surroundSelectedText(el, '<ul>\n', '\n</ul>');
+}
+
+function makeOrderedList(el) {
+    surroundSelectedText(el, '<ol>\n', '\n</ol>');
+}
+
+function makeListItem(el) {
+    surroundSelectedText(el, '<li>', '</li>');
+}
+
+function makeDel(el) {
+    surroundSelectedText(el, '<del>', '</del>');
 }
 
 function makeCode(el) {
     surroundSelectedText(el, '<code class="no-highlight">', '</code>');
-    return false;
 }
 
 function makeCodeBlock(el) {
     surroundSelectedText(el, '<pre><code class="no-highlight">', '</code></pre>');
-    return false;
 }
 
 function makeImageBox(el) {
@@ -159,13 +186,10 @@ function makeImageBox(el) {
     var val = el.value;
     var text = val.slice(sel.start, sel.end);
     var newtext = '<a href="' + text + '" class="imagebox">\n<img src="' + text + '" />\n</a>';
+    var scrollposition = el.scrollTop;
     el.value = val.slice(0, sel.start) + newtext + val.slice(sel.end);
+    el.scrollTop = scrollposition;
     setCaretPosition(el, sel.start + newtext.length);
-    return false;
-}
-
-function $$$(id) {
-    return document.getElementById(id);
 }
 
 function insertTag(tag) {
@@ -176,19 +200,17 @@ function insertTag(tag) {
     } else {
         el.value += ', ' + tag;
     }
-    return false;
 }
 
 function updatePreview() {
-    var container = $$$('preview');
-    container.innerHTML = '<div class="adminbox"><a href="#" onClick="return hidePreview();">ausblenden</a></div><h2>' + $$$('title').value + '</h2>' + $$$('contenttextarea').value + $$$('extendedtextarea').value + '<div style="clear: both;">&nbsp;</div>';
-    $('code').each(function(i, e) {hljs.highlightBlock(e)});
-    container.style.display = 'block';
+    var container = $('#preview');
+    container.html('<div class="adminbox"><a href="#" onClick="hidePreview(); return false;">ausblenden</a></div><h2>' + $('#title').val() + '</h2>' + $('#content').val() + $('#extended').val() + '<div style="clear: both;">&nbsp;</div>');
+    $('code', container).each(function(i, e) {hljs.highlightBlock(e)});
+    container.show();
 }
 
 function hidePreview() {
-    var container = $$$('preview');
-    container.innerHTML = '';
-    container.style.display = 'none';
-    return false;
+    var container = $('#preview');
+    container.html('');
+    container.hide();
 }
