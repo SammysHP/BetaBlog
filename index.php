b4382f7f84450d3dbb260751f01c0f1b67a78f7d
SammysHP <sven@sammyshp.de>
2017-03-06 19:10:16 +0100

    Silence some notices

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ require_once('lib/autoloader.php');
 
 setlocale(LC_ALL, 'de_DE@euro', 'de_DE', 'de');
 
+error_reporting(E_ALL & ~E_STRICT & ~E_NOTICE);
+
 respond('\controllers\Environment::initialize');
 
 
